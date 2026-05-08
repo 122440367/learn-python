48fd3f4fdaa7a020df2f073f0963e3bb94472d4e
122440367 <1224403167@qq.com>
2026-05-08 15:32:39 +0800

    字符串格式化

--- course1/course1.3.py
+++ course1/course1.3.py
@@ -20,3 +20,19 @@ print(st4)
 # \n 换行
 # \t 制表符
 # \" 双引号
+
+# 字符串连接
+print(st1 + " " + st4)  # 输出 Hello World! It's a good day!
+# 特别注意：字符串连接时，必须确保连接的对象都是字符串类型，否则会报错
+
+# 字符串转换 str()
+# eg
+num = 123
+print(str(num) + "转换成为字符串") # 将整数转换为字符串并拼接
+
+# 字符串的格式化
+name = "Alice"
+print("My name is %s." % name)  # 使用 %s 占位符格式化字符串
+# 使用 f"内容{变量}" 进行格式化
+print(f"My name is {name}.") # 使用 f-string 格式化字符串
+
